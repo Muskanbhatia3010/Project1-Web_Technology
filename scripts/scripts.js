e683d2b02f342422d573f460657f24d542c2cb44
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2024-12-03 22:46:58 -0800

    added menu button for mobile - 1 bug to fix - if you press menu in mobile to show, then hide it, then go back to desktop width, left menu nav won't show

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const oldEditor = document.getElementById('css-editor-old');
-    const motionPathEditor = document.getElementById('css-editor-motion-path');
-    // Function to handle CSS injection for each editor
-    function updateStyles(editor, styleId) {
-        let styleTag = document.getElementById(styleId);
+document.addEventListener("DOMContentLoaded", () => {
+  const oldEditor = document.getElementById("css-editor-old");
+  const motionPathEditor = document.getElementById("css-editor-motion-path");
+  // Function to handle CSS injection for each editor
+  function updateStyles(editor, styleId) {
+    let styleTag = document.getElementById(styleId);
 
-        // If the style tag doesn't exist, create it
-        if (!styleTag) {
-            styleTag = document.createElement('style');
-            styleTag.id = styleId;
-            document.head.appendChild(styleTag);
-        }
-
-        // Apply the CSS typed in the editor to the style tag
-        styleTag.textContent = editor.value;
+    // If the style tag doesn't exist, create it
+    if (!styleTag) {
+      styleTag = document.createElement("style");
+      styleTag.id = styleId;
+      document.head.appendChild(styleTag);
     }
 
-    // Event listener for the old animation editor
-    oldEditor.addEventListener('input', () => {
-        updateStyles(oldEditor, 'dynamic-styles-old');
-    });
+    // Apply the CSS typed in the editor to the style tag
+    styleTag.textContent = editor.value;
+  }
 
-    // Event listener for the motion path animation editor
-    motionPathEditor.addEventListener('input', () => {
-        updateStyles(motionPathEditor, 'dynamic-styles-motion-path');
-    });
+  // Event listener for the old animation editor
+  oldEditor.addEventListener("input", () => {
+    updateStyles(oldEditor, "dynamic-styles-old");
+  });
+
+  // Event listener for the motion path animation editor
+  motionPathEditor.addEventListener("input", () => {
+    updateStyles(motionPathEditor, "dynamic-styles-motion-path");
+  });
+});
+
+// Hamburger menu
+
+const toggleMenuButton = document.getElementById("toggle-menu");
+let isMenuOpen = false;
+
+toggleMenuButton.addEventListener("click", () => {
+  isMenuOpen = !isMenuOpen;
+  if (isMenuOpen) {
+    document.getElementById("navigation").style.display = "block";
+  } else {
+    document.getElementById("navigation").style.display = "none";
+  }
 });
